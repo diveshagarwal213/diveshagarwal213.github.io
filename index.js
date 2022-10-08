62431f3c3a95ceb1283a3603b4b6ad9ca67ed6e4
Divesg Aggarwal <diveshagarwal213@gmail.com>
2022-10-08 14:49:18 +0530

    updated about me

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
+}
 window.addEventListener("scroll", reveal);
 
-function openCity(evt,cityName) {
+function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("waw_tab_cont");
   for (i = 0; i < tabcontent.length; i++) {
-  tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-  tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
@@ -39,22 +39,60 @@ function openCity(evt,cityName) {
   When the user scrolls up, show the navbar 
 */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("nav_bar").style.top = "0";
-    document.getElementById("nav_bar").style.boxShadow = "0 10px 30px -10px var(--nav-shadow)";
+    document.getElementById("nav_bar").style.boxShadow =
+      "0 10px 30px -10px var(--nav-shadow)";
     document.getElementById("nav_bar").style.height = "70px";
-    if(currentScrollPos <= 5) {
+    if (currentScrollPos <= 5) {
       document.getElementById("nav_bar").style.boxShadow = "none";
       document.getElementById("nav_bar").style.height = "100px";
-    } 
+    }
   } else {
     document.getElementById("nav_bar").style.height = "70px";
     document.getElementById("nav_bar").style.top = "-70px";
   }
   prevScrollpos = currentScrollPos;
+};
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
 }
+function setExpTime() {
+  document.getElementById("expTime").textContent = timeSince(
+    new Date("Mon Apr 18 2022 14:10:38 GMT+0530")
+  );
+}
+
+window.onload = function afterWebPageLoad() {
+  setExpTime();
+  // Function to be executed
+};
 
 /* const printSentence = (id, sentence, speed = 50) => {
     let index = 0;
@@ -74,8 +112,8 @@ window.onscroll = function() {
       }
     }, speed);
 } */ // printSentence
-  
-  /* printSentence(
+
+/* printSentence(
     'typeEffect',
     ' "In this universe Life has an end but, <br> in Life <span class="txt-p" >Learning</span> never ends" ',
     50
